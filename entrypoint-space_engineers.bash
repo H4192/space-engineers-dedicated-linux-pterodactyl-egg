ad3e9b2bc92f1184e9e7dbb025a1392b73f131cf
h4192 <markus.selin@llive.fi>
2021-02-15 22:26:07 +0200

    add missing file

---
+++ entrypoint-space_engineers.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
+env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/container/wineprefix wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
