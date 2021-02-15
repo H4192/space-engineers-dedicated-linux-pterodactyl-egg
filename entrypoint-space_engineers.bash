d0749a064cdca1ee5419b28afefbd96c6d25ec7c
h4192 <markus.selin@llive.fi>
2021-02-15 04:27:25 +0200

    path fixes

--- entrypoint-space_engineers.bash
+++ entrypoint-space_engineers.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-source ~/.profile
-source ~/.bash_profile
+
 cd /home/container/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
-env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/wineprefix wine /home/container/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\home\\container\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
+env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/container/wineprefix wine /home/container/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\home\\container\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
